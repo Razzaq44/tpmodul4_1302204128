9ffbb33e440c664b82173777916900e354fb54f2
razzaq <razzaq@student.telkomuniversity.ac.id>
2022-03-18 15:04:24 +0700

    menambahkan class DataGeneric

--- tpmodul4_1302204128/Program.cs
+++ tpmodul4_1302204128/Program.cs
@@ -1,1 +1,28 @@
 ﻿
+using System;
+
+namespace tp4
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            DataGeneric<int> nim = new DataGeneric<int>(1302204128);
+            nim.Printdata();
+        }
+    }
+
+    class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T nim)
+        {
+            data = nim;
+        }
+        public void Printdata()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + this.data);
+        }
+    }
+}
