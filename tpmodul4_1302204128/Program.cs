6a34fd5fb01f564a7f682aa0d3eed14b8284ae0c
razzaq <razzaq@student.telkomuniversity.ac.id>
2022-03-18 14:34:31 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul4_1302204128/Program.cs
+++ tpmodul4_1302204128/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+namespace tp4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric.SapaUser("razzaq");
+        }
+    }
+
+    class HaloGeneric
+    {
+        public static void SapaUser<T>(T inputUser)
+        {
+            Console.WriteLine("Halo User " + inputUser);
+        }
+    }
+}
